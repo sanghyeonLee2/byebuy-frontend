0d18f4c9fc298e4e8fec421f50d05d209365e387
eggmun <eggmun98@gmail.com>
2025-01-11 20:53:05 +0900

    refactor: media 함수 모바일 전용으로 사이즈 조정

--- src/commons/utils/media.js
+++ src/commons/utils/media.js
@@ -1,14 +1,14 @@
 export const breakpoints = {
-  xs: '360px',
-  sm: '568px',
-  md: '768px',
-  lg: '992px',
-  xl: '1280px',
-  xxl: '1920px',
-  xxxl: '2300px',
+  xs: '320px',
+  sm: '360px',
+  md: '375px',
+  lg: '390px',
+  xl: '412px',
+  xxl: '428px',
+  xxxl: '480px',
 };
 
-// NOTE: 특정 구간에만 적용되는 미디어쿼리
+// NOTE: 특정 구간에만 적용
 export const media = {
   xs: `@media (min-width: ${breakpoints.xs}) and (max-width: ${breakpoints.sm})`,
   sm: `@media (min-width: ${breakpoints.sm}) and (max-width: ${breakpoints.md})`,
@@ -19,7 +19,7 @@ export const media = {
   xxxl: `@media (min-width: ${breakpoints.xxxl})`,
 };
 
-// NOTE: 특정 크기 이상에서 적용
+// NOTE: 특정 크기 이상
 export const mediaUp = {
   xs: `@media (min-width: ${breakpoints.xs})`,
   sm: `@media (min-width: ${breakpoints.sm})`,
@@ -30,7 +30,7 @@ export const mediaUp = {
   xxxl: `@media (min-width: ${breakpoints.xxxl})`,
 };
 
-// NOTE: 특정 크기 이하에서 적용
+// NOTE: 특정 크기 이하
 export const mediaDown = {
   xs: `@media (max-width: ${breakpoints.xs})`,
   sm: `@media (max-width: ${breakpoints.sm})`,
