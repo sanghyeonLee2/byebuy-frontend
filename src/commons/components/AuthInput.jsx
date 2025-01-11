b27a027d2829d8c9df11f9622cf0a46b078926ac
SanghyeonLee <hyeon012366@gmail.com>
2025-01-12 00:06:17 +0900

    feat: 조건부 로직 추가가

--- src/commons/components/AuthInput.jsx
+++ src/commons/components/AuthInput.jsx
@@ -8,8 +8,8 @@ function AuthInput({ label, type, placeholder }) {
       <Label>{label}</Label>
       <InputWrap>
         <Input type={type} placeholder={placeholder} />
-        {type === 'password' && <Icon src={iconMap.check} />}
-        {type === 'email' && <Icon src={iconMap.me} />}
+        {type === 'password' && <Icon src={iconMap.check} alt="비밀번호 체크" />}
+        {type === 'email' && <Icon src={iconMap.me} alt="아이디 체크" />}
       </InputWrap>
     </>
   );
