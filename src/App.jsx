57ed67c365b3df296cc583839ff287a7efcc4699
eggmun <eggmun98@gmail.com>
2025-01-12 04:54:45 +0900

    feat: 매칭 페이지 등록 및 페이지 패치 주소 변경

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import HeaderNavigation from './components/HeaderNavigation';
-import Example from './pages/example/Example123';
-import Home from './pages/home/HomePage';
-import { Register } from './pages/product/register/Register';
+import AuthPage from './pages/auth/AuthPage';
 import ChatPage from './pages/chat/ChatPage';
 import ChatListPage from './pages/chatList/ChatListPage';
-import SigninPage from './pages/signin/SigninPage';
-import AuthPage from './pages/auth/AuthPage';
-import SignupPage from './pages/signup/SignupPage';
+import Example from './pages/example/Example123';
 import ExplorationPage from './pages/exploration/ExplorationPage';
+import Home from './pages/home/HomePage';
+import MatchingPage from './pages/matching/matching';
+import { Register } from './pages/product/register/Register';
+import SigninPage from './pages/signin/SigninPage';
+import SignupPage from './pages/signup/SignupPage';
 
 function App() {
   return (
@@ -20,10 +21,11 @@ function App() {
         <Route path="/signup" element={<SignupPage />} />
         <Route path="/chatlist" element={<ChatListPage />} />
         <Route path="/chat" element={<ChatPage />} />
-        <Route path="/product/register" element={<Register />} />
+        <Route path="/register" element={<Register />} />
         <Route path="/signin" element={<SigninPage />} />
         <Route path="/auth" element={<AuthPage />} />
         <Route path="/product/exploration" element={<ExplorationPage />} />
+        <Route path="/matching" element={<MatchingPage />} />
       </Routes>
     </BrowserRouter>
   );
