b174676f34214315807bcd214190aebe8970d828
eggmun <eggmun98@gmail.com>
2025-01-12 09:16:31 +0900

    fix: 배포 시 이미지 안보이는 부분 수정

--- src/components/icons/iconMap.js
+++ src/components/icons/iconMap.js
@@ -1,15 +1,29 @@
+import accountCircle from './account_circle.svg';
+import addAPhoto from './add_a_photo.svg';
+import arrowDropDown from './arrow_drop_down.svg';
+import backArrow from './back-arrow.svg';
+import chat from './chat.svg';
+import check from './check.svg';
+import empty from './empty.svg';
+import filterList from './filter_list.svg';
+import home from './home.svg';
+import kebab from './kebab.svg';
+import logo from './logo.svg';
+import me from './me.svg';
+import myProduct from './my_product.svg';
+
 export const iconMap = {
-  backArrow: '/icons/back-arrow.svg',
-  kebab: '/icons/kebab.svg',
-  empty: '/icons/empty.svg',
-  addAPhoto: '/icons/add_a_photo.svg',
-  check: '/icons/check.svg',
-  me: '/icons/me.svg',
-  arrowDropDown: '/icons/arrow_drop_down.svg',
-  filerList: '/icons/filter_list.svg',
-  myProduct: '/icons/my_product.svg',
-  logo: '/icons/logo.svg',
-  home: '/icons/home.svg',
-  chat: '/icons/chat.svg',
-  accountCircle: '/icons/account_circle.svg',
+  backArrow,
+  kebab,
+  empty,
+  addAPhoto,
+  check,
+  me,
+  arrowDropDown,
+  filterList,
+  myProduct,
+  logo,
+  home,
+  chat,
+  accountCircle,
 };
